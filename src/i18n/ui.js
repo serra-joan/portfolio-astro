a7f06b7ebcf8c51340d55b8f261c140db4ce22de
serra-joan <joanserra0501@gmail.com>
2024-07-19 12:34:47 +0200

    Added multilanguague (es, ca, en)

---
+++ src/i18n/ui.js
@@ -0,0 +1,16 @@
+export const languages = {
+    ['es'] : {
+        route: "/",
+        label: "Español",
+    },
+    ['en'] : {
+        route: "/en",
+        label: "English",
+    },
+    ['ca'] : {
+        route: "/ca",
+        label: "Català",
+    }
+}
+
+// Execute "npx astro-i18next generate" For the languages to be generated
